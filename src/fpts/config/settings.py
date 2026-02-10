277c2a191859fd5c6e936b4e5d014b42bb3a0d79
Milan <work.milandave@gmail.com>
2026-02-10 12:47:20 +0000

    Feature(database): Add libraries for postgres and postGIS, as well as database data source name param in config settings

--- src/fpts/config/settings.py
+++ src/fpts/config/settings.py
@@ -17,4 +17,6 @@ class Settings(BaseSettings):
     mod13q1_collection: str = "modis-13Q1-061"
     mod13q1_ndvi_asset_key: str = "250m_16_days_NDVI"
 
+    database_dsn: str = "postgresql://postgres:postgres@localhost:5432/fpts"
+
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
